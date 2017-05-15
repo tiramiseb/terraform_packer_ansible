de599a44efc7ab609914f520a559c66fed05cd81
Sébastien Maccagnoni-Munch <sebastien@maccagnoni.eu>
2017-05-15 15:55:09 +0200

    Generate the "locally" source from a template
    So that it contains credentials as env vars, for use with Packer

--- initialize.sh
+++ initialize.sh
@@ -27,13 +27,22 @@ fi
 
 cd ..
 
-if [ -f authentication.tf ]
-then
-    echo "    → AWS authentication file already exists, modify it manually if needed"
-else
-    read -p "Please provide your AWS access key: " awskey
-    read -p "Please provide your AWS secret key: " awssecret
+echo "    → Initializing secrets, etc"
 
-    sed "s|ACCESS_KEY_HERE|$awskey|;s|SECRET_KEY_HERE|$awssecret|" authentication.tf.sample >> authentication.tf
+. ./locally 2>/dev/null
+
+if [ -z "$AWS_ACCESS_KEY" ]
+then
+    read -p "Please provide your AWS access key (see the IAM service): " AWS_ACCESS_KEY
 fi
+if [ -z "$AWS_SECRET_KEY" ]
+then
+    read -p "Please provide your AWS secret key (see the IAM service): " AWS_SECRET_KEY
+fi
+if [ -z "$AWS_LOGIN_SERVER" ]
+then
+    read -p "Please provide your AWS ECR login server (see the ECS service): " AWS_LOGIN_SERVER
+fi
+sed "s|ACCESS_KEY_HERE|$AWS_ACCESS_KEY|;s|SECRET_KEY_HERE|$AWS_SECRET_KEY|" authentication.tf.template > authentication.tf
+sed "s|ACCESS_KEY_HERE|$AWS_ACCESS_KEY|;s|SECRET_KEY_HERE|$AWS_SECRET_KEY|;s|LOGIN_SERVER_HERE|$AWS_LOGIN_SERVER|" locally.template > locally
 
